237f958867b5ac45313b386b17b6aab704c71d8b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 13:53:11 +0200

    changes

--- routes/user/index.dart
+++ routes/user/index.dart
@@ -11,9 +11,22 @@ Future<Response> _getUsers() {
   return Future.value(
     Response.json(
       body: [
-        {'name': 'Tamani Sam'},
-        {'name': 'Troy Mac'},
+        {'name': 'Tamani Sam', 'email': 'tamani@gmail.com'},
+        {'name': 'Troy Mac', 'email': 'troy@troymac.com'},
       ],
     ),
+  );
+}
+
+Future<Response> _createUser(RequestContext context) async {
+  final json = (await context.request.json()) as Map<String, dynamic>;
+  final name = json['name'];
+  final email = json['email'];
+  return Response.json(
+    body: {
+      'message': 'User created',
+      'name': name,
+      'email': email,
+    },
   );
 }
