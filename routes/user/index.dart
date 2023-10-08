d629e864ce3e65346badf4c76ec0246282ebf543
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 13:42:28 +0200

    changes

---
+++ routes/user/index.dart
@@ -0,0 +1,12 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => _getUsers(),
+    _ => Future.value(Response.json(body: 'Default method'))
+  };
+}
+
+Future<Response> _getUsers() {
+  return Future.value(Response.json());
+}
